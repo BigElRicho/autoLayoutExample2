277ff34c40ad1ac006a32797188a85e8a5dc2b41
Julian Rich <s3407957@student.rmit.edu.au>
2020-09-23 18:10:06 +1000

    added a whole bunch of updates and images.

---
+++ autoLayoutExample2/Model/Characters.swift
@@ -0,0 +1,44 @@
+//
+//  Characters.swift
+//  autoLayoutExample2
+//
+//  Created by Julian Rich on 23/9/20.
+//  Copyright © 2020 Julian Rich. All rights reserved.
+//
+
+import Foundation
+
+struct Characters
+{
+    private var characters:[String] = []
+    private var current:Int = 0
+    
+    private let birthday:CustomDate = CustomDate(day:3, month:8, year:2019)
+    
+    init()
+    {
+        populateCharacters()
+    }
+    
+    private mutating func populateCharacters()
+    {
+        characters.append("simba_1")
+        characters.append("lion_king_2")
+        characters.append("lion_king_3")
+        characters.append("lion_king_4")
+  
+    }
+    
+    public mutating func getNextImageName()-> String
+    {
+        if current == 3
+        {
+            current = 0
+        }
+        else
+        {
+            current += 1
+        }
+        return characters[current]
+    }
+}
---
+++ autoLayoutExample2/Model/CustomDate.swift
@@ -0,0 +1,50 @@
+//
+//  customDate.swift
+//  autoLayoutExample2
+//
+//  Created by Julian Rich on 23/9/20.
+//  Copyright © 2020 Julian Rich. All rights reserved.
+//
+
+import Foundation
+
+struct CustomDate
+{
+    
+    private var date:Date?
+    
+    init(day:Int, month:Int, year:Int, hour:Int = 0, minute:Int = 0,
+         timeZone:TimeZone = .current)
+    {
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        dateComponents.timeZone = timeZone
+        dateComponents.minute = minute
+        dateComponents.hour = hour
+        
+        let userCalandar = Calendar.current
+        
+        date = userCalandar.date(from: dateComponents)
+        
+    }
+    
+    //as format creates a formatted String type.
+    //-> is the return value type.
+    func formatted(as format:String = "dd-MM-yyyy") -> String?
+    {
+        //This attempts to create a value called date with the customDates date variable.
+        //It returns nil if there is none.
+        guard let date = self.date else {return nil}
+        
+        //This creates the return argument from the input argument above.
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        
+        return dateFormatter.string(from: date)
+        
+        
+        
+    }
+}
